37c3f6dbabb817b483e1fc818dbfb819bda78d34
yigs <yigsvnsla@gmail.com>
2024-04-08 02:53:15 -0500

    add compound components

--- apps/corrier-admin/src/routes/dashboard/layout.tsx
+++ apps/corrier-admin/src/routes/dashboard/layout.tsx
@@ -1,11 +1,38 @@
 import { Slot, component$ } from "@builder.io/qwik";
-import Aside from "~/components/aside";
 import Header from "~/components/header";
+import { Aside } from "~/components/aside";
+import { Icons } from "~/components/icons";
 
 export default component$(() => (
   <section class="bg-gray-50 antialiased dark:bg-gray-900">
     <Header />
-    <Aside />
+
+    <Aside>
+      <Aside.Header>Corrier Admin</Aside.Header>
+      <Aside.Content>
+        <Aside.Section>
+          <Aside.Item href="sign-in">
+            <Icons.ChartPie></Icons.ChartPie>
+            <span class="ml-3">item 1</span>
+          </Aside.Item>
+          <Aside.Item href="sign-in">
+            <Icons.ChartPie></Icons.ChartPie>
+            <span class="ml-3">item 1</span>
+          </Aside.Item>
+          <Aside.Item href="sign-in">
+            <Icons.ChartPie></Icons.ChartPie>
+            <span class="ml-3">item 1</span>
+          </Aside.Item>
+          <Aside.DropDown>
+            <Aside.DropDown.Content>
+              
+            </Aside.DropDown.Content>
+          </Aside.DropDown>
+        </Aside.Section>
+      </Aside.Content>
+      <Aside.Footer></Aside.Footer>
+    </Aside>
+
     <main class="h-auto p-4 pt-20 md:ml-64">
       <Slot></Slot>
     </main>
